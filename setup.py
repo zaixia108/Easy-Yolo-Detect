90eccfe22d0172753bdb7ded2c871be55b7bd465
zaixia108 <58163333+zaixia108@users.noreply.github.com>
2025-02-11 14:08:35 +0800

    无需多次编译

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='EasYoloD',
-    version='0.1.3',
+    version='0.1.5',
     description='easyolo python library',
     long_description=long_description,
     long_description_content_type="text/markdown",
